5e41754a41b6eee049e770cd3b6f9271dcb07e98
bhimatak <bhima.t@gmail.com>
2022-07-09 17:52:40 +0530

    Updated

--- day07/src/sysPrg4.cpp
+++ day07/src/sysPrg4.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
 
 using namespace std;
 
 int main()
 {
-	cout<<"Run ps -el command"<<endl;
-	sleep(50);
-	cout<<"Run ps -el command once again"<<endl;
-	for(;;);
-		
+	int i=0, j=0, pid;
+	pid = fork();
+	if(pid == 0)
+	{
+		for(i=0;i<1000;i++)
+			cout<<"i="<<i<<endl;
+	}
+	else
+	{
+		if(pid>0){
+			wait(0); // Process Sync
+			for(j=0;j<1000;j++)
+				cout<<"j="<<j<<endl;
+		}
+	}
+
 	return 0;
 }
