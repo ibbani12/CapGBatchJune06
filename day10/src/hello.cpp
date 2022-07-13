39a191065d1e8526e31386c80d9f38b543189eac
bhimatak <bhima.t@gmail.com>
2022-07-13 12:43:09 +0530

    Updated

--- day10/src/hello.cpp
+++ day10/src/hello.cpp
@@ -1,10 +1,13 @@
 #include <myheader.h>
 
-int main()
+int main(int argc, char *argv[])
 {
 	cout<<"Hello!, Good Day"<<endl;
 	cout<<"Hello PID: "<<getpid()<<endl;
 	cout<<"Hello PPID: "<<getppid()<<endl;
+	for(int i=0;i<argc;i++)
+	 	cout<<"argv["<<i<<"] = "<<argv[i]<<endl;
+
 	cout<<"Exec ends"<<endl;
 	return 0;
 }
--- day10/src/sysPrg2.cpp
+++ day10/src/sysPrg2.cpp
@@ -1,11 +1,15 @@
 #include <myheader.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	char* temp[3];
+	temp[0]= argv[1];
+	temp[1]= argv[2];
+	//temp[2]= (char*)0;
 	cout<<"\nBefore exec sys call PID: "<<getpid()<<endl;
 	cout<<"\nParent PID: "<<getppid()<<endl;
 	cout<<"\nExec starts"<<endl;
-	execl("/home/bhima/CapGJune6/CapGBatchJune06/day10/hello", "hello",(char *)0);
+	execv("/home/bhima/CapGJune6/CapGBatchJune06/day10/hello",argv);
 	cout<<"\nThis will not print"<<endl;
 
 	return 0;
