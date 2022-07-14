5bf958cb94bd8472321e67bec99f06974f87c764
bhimatak <bhima.t@gmail.com>
2022-07-14 12:25:12 +0530

    Updated

---
+++ day12/src/sysPrg2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <csignal>
+
+using namespace std;
+
+static void signalHandler(int ID)
+{
+	cout<<"ID : "<<ID<<endl;
+}
+
+void RegisterSignal()
+{
+	signal(SIGINT, signalHandler);
+	signal(SIGILL, signalHandler);
+	signal(SIGFPE, signalHandler);
+	signal(SIGSEGV, signalHandler);
+	signal(SIGTERM, signalHandler);
+	signal(SIGABRT, signalHandler);
+}
+
+
+int main()
+{
+	RegisterSignal();
+	for(;;);
+		
+	return 0;
+}
