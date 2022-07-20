2a462730a875683284197a2db85dc2610f737b57
bhimatak <bhima.t@gmail.com>
2022-07-20 16:34:38 +0530

    Updated

--- day15/src/a1.cpp
+++ day15/src/a1.cpp
@@ -1,5 +1,26 @@
 #include <myheader.h>
 
+static void process_display_exit_code(int exitstatus)
+{
+	cout<<"Interrupt signal ("<<exitstatus<<") received"<<endl;
+}
+
+static void signalHandler(int ID)
+{
+	cout<<"ID : "<<ID<<endl;
+}
+
+void RegisterSignal()
+{
+	signal(SIGINT, process_display_exit_code);
+	signal(SIGILL, process_display_exit_code);
+	signal(SIGFPE, process_display_exit_code);
+	signal(SIGSEGV, process_display_exit_code);
+	signal(SIGTERM, process_display_exit_code);
+	signal(SIGABRT, process_display_exit_code);
+	signal(SIGCHLD, process_display_exit_code);
+}
+
 int main(int argc, char *argv[])
 {
 	int pid, status;
@@ -10,11 +31,18 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	
-	//char *fName = argv[1];
+	char *fName = argv[1];
 
 	
 	
 	
+					// signal(SIGINT, process_display_exit_code);
+					// signal(SIGILL, process_display_exit_code);
+					// signal(SIGFPE, process_display_exit_code);
+					// signal(SIGSEGV, process_display_exit_code);
+					// signal(SIGTERM, process_display_exit_code);
+					// signal(SIGABRT, process_display_exit_code);
+					// signal(SIGCHLD, process_display_exit_code);
 	pid = fork();
 	if(pid<0){
 		perror("fork() error");
@@ -25,16 +53,20 @@ int main(int argc, char *argv[])
 		cout<<"Child pid: "<<getpid()<<endl;
 		cout<<"Child's Parent pid: "<<getppid()<<endl;
 		cout<<"\nEnter two lines for the input:"<<endl;
-		sleep(20);
+		Write2File(fName);
+		sleep(30);
 		exit(EXIT_FAILURE);
-		//Write2File(fName);
+		
 
 	}
 	else
 	{
 		do{
-			if((pid = waitpid(pid,&status,WNOHANG)) == -1)
+			if((pid = waitpid(pid,&status,0)) == -1)
+			{
+				signal(SIGCHLD, signalHandler);
 				perror("wait() error");
+			}
 			else if(pid == 0){
 				time(&t);
 				cout<<"\nChild is still running at "<<ctime(&t)<<endl;
@@ -42,15 +74,23 @@ int main(int argc, char *argv[])
 			}
 			else
 			{
-				if(WIFEXITED(status))
+				signal(SIGCHLD, process_display_exit_code);
+				if(WIFEXITED(status)){
+					
 					cout<<"child exited with the status : "<<WEXITSTATUS(status)<<endl;
+				}
+				else if(WIFSIGNALED(status)){
+					signal(SIGCHLD, process_display_exit_code);
+					cout<<"Signaled: "<<status<<endl;
+				}
 				else
 					cout<<"Child did not exit successfully"<<endl;
 				cout<<"Parent pid: "<<getpid()<<endl;
 				cout<<"Parent's Parent pid: "<<getppid()<<endl;
 				cout<<"\nRead the contents from input file"<<endl;
+				ReadFromFile(fName);
 			}
-			//ReadFromFile(fName);
+			
 		}while(pid == 0);
 
 	}
